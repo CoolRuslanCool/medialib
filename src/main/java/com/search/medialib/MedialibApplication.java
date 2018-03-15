c838c6978c541c7af6c0dd6e6e66628cdd20639e
CoolRuslanCool <cool3331982@mail.ru>
2018-03-15 17:42:39 +0300

    boot react template

---
+++ src/main/java/com/search/medialib/MedialibApplication.java
@@ -0,0 +1,12 @@
+package com.search.medialib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedialibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+}
